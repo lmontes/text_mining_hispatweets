0ce40df05956ca3dc208cd03763c7f00d91b03e7
Luis Miguel Montes <Luis Miguel>
2016-08-21 12:52:39 +0200

    Reorganizamos fichero usado para el entrenamiento de los modelos

--- R/check.R
+++ R/check.R
@@ -1,21 +1,56 @@
 library("e1071")
+library("rpart")
 
-# Genero
+# Cargamos los datasets
 train_ge <- read.table("../data/train_ge", quote="\"")
 test_ge <- read.table("../data/test_ge", quote="\"")
 
+train_co <- read.table("../data/train_co", quote="\"")
+test_co <- read.table("../data/test_co", quote="\"")
+
+
+# Genero - NB con BoW (0.3368132)
 model_ge <- naiveBayes(V1 ~ ., data = train_ge)
 pred_ge <- predict(model_ge, test_ge[,2:length(test_ge)])
 
 accuracy_ge <- sum(diag(table(pred_ge, test_ge[,1]))) / length(test_ge[,1])
 accuracy_ge
 
-# Pais
-train_co <- read.table("../data/train_co", quote="\"")
-test_co <- read.table("../data/test_co", quote="\"")
+# Genero - Arboles clasificacion con BoW (0.4802198)
+model_ge <- rpart(V1 ~ ., method="class", data = train_ge)
+pred_ge <- predict(model_ge, test_ge[,2:length(test_ge)], type = "class")
 
+accuracy_ge <- sum(diag(table(pred_ge, test_ge[,1]))) / length(test_ge[,1])
+accuracy_ge
+
+
+# Genero - NB sin BoW (0.4005495)
+train <- train_ge[,999:length(train_ge)]
+train <- cbind(train_ge[,1], train)
+
+colnames(train)[1] <- "clase"
+
+test <- test_ge[,999:length(test_ge)]
+test <- cbind(test_ge[,1], test)
+
+model <- naiveBayes(clase ~ ., data = train)
+pred <- predict(model, test[,2:length(test)])
+
+accuracy <- sum(diag(table(pred, test[,1]))) / length(test[,1])
+accuracy
+
+
+# Pais - NB con BoW (0.8516484)
 model_co <- naiveBayes(V1 ~ ., data = train_co)
 pred_co <- predict(model_co, test_co[,2:length(test_co)])
+
+accuracy_co <- sum(diag(table(pred_co, test_co[,1]))) / length(test_co[,1])
+accuracy_co
+
+
+# Pais - Arboles de clasificacion con BoW (0.8346154)
+model_co <- rpart(V1 ~ ., method="class", data = train_co)
+pred_co <- predict(model_co, test_co[,2:length(test_co)], type = "class")
 
 accuracy_co <- sum(diag(table(pred_co, test_co[,1]))) / length(test_co[,1])
 accuracy_co
@@ -33,17 +68,4 @@ accuracy_co
 
 
 
-train <- train_ge[,1001:length(train_ge)]
-train <- cbind(train_ge[,1], train)
 
-colnames(train)[1] <- "clase"
-
-test <- test_ge[,1001:length(test_ge)]
-test <- cbind(test_ge[,1], test)
-
-
-model <- naiveBayes(clase ~ ., data = train)
-pred <- predict(model, test[,2:length(test)])
-
-accuracy <- sum(diag(table(pred, test[,1]))) / length(test[,1])
-accuracy
